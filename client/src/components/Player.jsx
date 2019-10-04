0e3fca0386876c5cc143452a41e5e6e2e2c444a2
DimejiAre <olardi2@gmail.com>
2019-10-04 09:04:42 +0100

    create Player component

---
+++ client/src/components/Player.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Player extends React.Component {
+    render(){
+        const {player} = this.props;
+        return(
+            <div>
+                <h3>{player.name}</h3>
+                <p>{"Country: " + player.country}</p>
+                <p>{"Searches: " + player.searches}</p>
+            </div>
+        )
+    }
+}
+
+export default Player;
